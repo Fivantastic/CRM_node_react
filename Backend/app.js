d045bb66a8c824cbb2baa842da6b336baeb9efbb
Ivan <ivan.asencio34@gmail.com>
2024-04-16 18:29:13 +0200

    middleware 404 y gestion de errores

--- Backend/app.js
+++ Backend/app.js
@@ -6,9 +6,23 @@ import { PORT } from './env.js';
 // Crear el servidor
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(fileUpload());
 app.use(morgan('dev'));
+
+
+
+// Middleware 404 Not Found
+app.use((req, res, next) => {
+    res.status(404).json({ error: 'Not Found' });
+});
+
+// Middleware de Gestión de Errores
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).json({ error: 'Internal Server Error' });
+});
 
 //Ponemos el servidor a escuchar
 app.listen(PORT, () => {

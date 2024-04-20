b121dc19241d9d5abc6c60aea2426fe0563e261f
Daniel Montero Castro <salamapatela@gmail.com>
2024-04-20 17:09:28 +0200

    Endpoit Listdo clientes

--- Backend/src/routes/customerRoutes.js
+++ Backend/src/routes/customerRoutes.js
@@ -2,13 +2,18 @@ import express from 'express';
 import { newCustomerControllers } from '../controllers/customer/newCustomerController.js';
 import { customerExists } from '../middlewares/customerExists.js';
 import { updateCustomerController } from '../controllers/customer/updateCustomerController.js';
+import { getCustomerListController } from '../controllers/customer/getCustomerController.js';
 
 // Creamos un router
 export const customerRouter = express.Router();
 
-// Ruta cliente
+// Agregar cliente
 customerRouter.post('/customer/register', newCustomerControllers);
 
+// Obtener lista de clientes
+customerRouter.get('/customer/list', getCustomerListController);
+
+// Modificar cliente
 customerRouter.put(
   '/customer/:customerId',
   customerExists,

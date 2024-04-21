cb39accd998d23d384d0c878739799c9dd63f9b0
Daniel <dani.v2509@outlook.com>
2024-04-21 16:33:43 +0200

    actualizar estado (activo)

---
+++ Backend/src/controllers/users/deactivateUserController.js
@@ -0,0 +1,21 @@
+import { toggleActivationService } from "../../services/toggleActivationService.js";
+
+export const deactivateUserController = async (req, res, next) => {
+
+    try {
+        // Obtenemos el id del usuario.
+        const userId =  req.params.id_user;
+
+        // Desactivamos al usuario en la base de datos.
+        const user = await toggleActivationService(userId)
+
+        // Devolvemos el usuario actualizado.
+        res.send({
+            status: 'ok',
+            message: `Estado del usuario cambiado a: ${user.active === 1 ? 'Activo' : 'Inactivo'}`,
+            data: { user },
+        });
+    } catch (error) {
+        next(error);
+    }
+}
--- Backend/src/routes/userRoutes.js
+++ Backend/src/routes/userRoutes.js
@@ -5,6 +5,7 @@ import { checkAdminPrivileges } from '../middlewares/checkAdminPrivileges.js';
 import { updateUserController } from '../controllers/users/updateUserController.js';
 import { loginUserControllers } from '../controllers/users/loginUserControllers.js';
 import { validateUserController } from '../controllers/users/validateUserControllers.js';
+import { deactivateUserController } from '../controllers/users/deactivateUserController.js';
 
 // Creamos un router
 export const userRouter = express.Router();
@@ -12,7 +13,9 @@ export const userRouter = express.Router();
 // Ruta user
 // userRouter.post('/user/register', authenticateUser, checkAdminPrivileges,  newUserController);
 
+// Test (sin permisos admin)
 userRouter.post('/user/register',  newUserController);
+userRouter.put('/user/toggleActivation/:id_user', deactivateUserController)
 
 userRouter.put("/user/validate/:registration_code", validateUserController);
 

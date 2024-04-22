ecdc47b6dae5f90050adb0099a9e540616a40e06
AlbertoGongora <gongorabada@gmail.com>
2024-04-22 12:44:19 +0200

    Enpoint envio de email para recuperar password

--- Backend/src/controllers/users/recoveryPasswordController .js
+++ Backend/src/controllers/users/recoveryPasswordController .js
@@ -5,56 +5,34 @@ import { updateUserModel } from '../../models/user/updateUserModel.js'; // Actua
 // import nodemailer from 'nodemailer'; // Comentado para desactivar Nodemailer
 import { success } from '../../utils/success.js';
 import { serverError } from '../../services/errorService.js';
+import { sendRecoveryPaswordEmail } from '../../services/emailService.js';
+import { recoveryPasswordService } from '../../services/recoveryPasswordService.js';
 
 export const recoveryPasswordController = async (req, res, next) => {
   try {
     // Validar el cuerpo de la solicitud
     await validateSchemaUtil(recoveryPasswordSchema, req.body);
+    console.log('Ha pasado el esquema');
 
     const { email } = req.body;
 
-    // Obtener el usuario por correo electrónico
-    const user = await selectUserByEmailModel(email);
+    console.log('Email:', email);
 
-    if (!user) {
-      return res.status(404).json({ error: 'El correo electrónico no está asociado a ninguna cuenta' });
-    }
+    const new_registration_code = await recoveryPasswordService(email);
 
-    // Generar un código de recuperación único
-    const recoveryCode = crypto.randomUUID();
+    console.log('New registration code en recoveryPasswordController:', new_registration_code);
 
-    // Actualizar el usuario con el código de recuperación
-    await updateUserModel(user.id_user, { recovery_code: recoveryCode });
+        // Comentado para evitar envío de correos electrónicos
+    // Enviar correo electrónico de cambio de contraseña
+    await sendRecoveryPaswordEmail(email, new_registration_code);
 
-    // Comentado para evitar envío de correos electrónicos
-    /*
-    const transporter = nodemailer.createTransport({
-      service: 'Gmail', // Servicio de correo
-      auth: {
-        user: 'tu_correo@gmail.com', // Correo electrónico
-        pass: 'tu_contraseña', // Contraseña
-      },
+    console.log('Correo enviado');
+    
+    // Devolvemos el usuario actualizado.
+    res.send({
+      status: 'ok',
+      message: 'Email enviado de cambio de contraseña'
     });
-
-    const mailOptions = {
-      from: 'tu_correo@gmail.com',
-      to: email,
-      subject: 'Recuperación de Contraseña',
-      text: `Por favor, haz clic en el siguiente enlace para restablecer tu contraseña: http://localhost:3000/reset-password/${recoveryCode}`,
-    };
-
-    transporter.sendMail(mailOptions, (err, info) => {
-      if (err) {
-        return next(serverError('No se pudo enviar el correo electrónico'));
-      }
-
-      return res.status(200).json(success({ msg: 'Se ha enviado un correo electrónico con instrucciones para restablecer la contraseña' }));
-    });
-    */
-
-    // Respuesta temporal sin envío de correos electrónicos
-    return res.status(200).json(success({ msg: 'Código de recuperación generado para restablecer la contraseña' }));
-
   } catch (error) {
     return next(serverError(error.message)); // Manejo de errores
   }

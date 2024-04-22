ede4ebbc0b165b1a4b87d98702058da385bfcdfe
Dorfo84 <ganondorfo@gmail.com>
2024-04-22 19:47:45 +0200

    endpoint listado productos

---
+++ Backend/src/controllers/products/productListController.js
@@ -0,0 +1,16 @@
+import { selectProductListModel } from "../../models/products/selectProductListModel.js";
+
+
+export const productListController = async (req, res, next) => {
+    try {
+      const listProducts = await selectProductListModel();
+  
+      res.status(200).send({
+        status: 'ok',
+        data: { listProducts },
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+  
--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import { newProductControllers } from '../controllers/products/newProductController.js';
+import { productListController } from '../controllers/products/productListController.js';
+
 
 export const productRouter = express.Router();
 
 productRouter.post('/product/register', newProductControllers);
+
+productRouter.get('/product/list', productListController);
+

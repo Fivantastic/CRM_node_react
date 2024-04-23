e7433be6f878c88e727c29f17b62e5c18848a769
Juan Carlos Varela <jcvarelag@gmail.com>
2024-04-23 12:52:45 +0200

    updateProdutEndpoint

--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -1,6 +1,11 @@
 import express from 'express';
 import { newProductControllers } from '../controllers/products/newProductController.js';
+import { updateProductController } from '../controllers/products/updateProductController.js';
 
 export const productRouter = express.Router();
 
+// Agregar producto
 productRouter.post('/product/register', newProductControllers);
+
+// Modificar Producto
+productRouter.put('/product/:id_product', updateProductController)

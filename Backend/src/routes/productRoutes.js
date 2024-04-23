a1f8d3eef94114ca713fdfb74d7116732418d915
Daniel Montero Castro <salamapatela@gmail.com>
2024-04-23 12:31:17 +0200

    insertar privilegios en registrar un producto y cambio de variables en serviceExist

--- Backend/src/routes/productRoutes.js
+++ Backend/src/routes/productRoutes.js
@@ -1,11 +1,16 @@
 import express from 'express';
 import { newProductControllers } from '../controllers/products/newProductController.js';
 import { productListController } from '../controllers/products/productListController.js';
-
+import { authenticateUser } from '../middlewares/authenticateUser.js';
+import { adminAuthMiddleware } from '../middlewares/adminAuthMiddleware.js';
 
 export const productRouter = express.Router();
 
-productRouter.post('/product/register', newProductControllers);
+productRouter.post(
+  '/product/register',
+  authenticateUser,
+  adminAuthMiddleware,
+  newProductControllers
+);
 
 productRouter.get('/product/list', productListController);
-

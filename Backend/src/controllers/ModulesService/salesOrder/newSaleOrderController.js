6cc0e46c560cb314ff4f844c9dd4f19c7bf33ce7
AlbertoGongora <gongorabada@gmail.com>
2024-04-23 18:37:46 +0200

    Mod en DB y creacion de carpeta SalesOrder

---
+++ Backend/src/controllers/ModulesService/salesOrder/newSaleOrderController.js
@@ -0,0 +1,13 @@
+
+
+// export const newSaleOrderController = async (req, res, next) => {
+//     try {
+//         //obtenemos el id_user del token
+//         const userId = req.user.id_user;
+
+//         const { id_product, quantity } = req.body;
+
+//     } catch (error) {
+
+//     }
+// }

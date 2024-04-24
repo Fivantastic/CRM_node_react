d1f0a5a617e0f0f6a77134cb273bff9dc7a79406
AlbertoGongora <gongorabada@gmail.com>
2024-04-24 10:33:19 +0200

    Generador contraseña mod, agregada visitas clientes

--- Backend/src/models/customer/selectCustomerByIdModel.js
+++ Backend/src/models/customer/selectCustomerByIdModel.js
@@ -3,14 +3,11 @@ import { getDBPool } from '../../db/getPool.js';
 export const selectCustomerByIdModel = async (id_customer) => {
   const pool = await getDBPool();
 
-  console.log('id_customer:', id_customer);
-
   // Comprobar si existe un cliente con el id proporcionado.
   const [customer] = await pool.query(
     `SELECT * FROM Customers WHERE id_customer = ?`,
     [id_customer]
   );
-  console.log('customer:', customer);
 
   return customer;
 };
--- Backend/src/routes/moduleRoutes.js
+++ Backend/src/routes/moduleRoutes.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { moduleExist } from '../middlewares/serviceExist.js';
 import { getModuleController } from '../controllers/ModulesService/getServiceController.js';
-// import { authenticateUser } from '../middlewares/authenticateUser.js';
-// import { newVisitController } from '../controllers/ModulesService/Visits/newVisitController.js';
+import { authenticateUser } from '../middlewares/authenticateUser.js';
+import { newVisitController } from '../controllers/ModulesService/Visits/newVisitController.js';
 // import { adminAuthMiddleware } from '../middlewares/adminAuthMiddleware.js';
 // import { getUserVisitsController } from '../controllers/ModulesService/Visits/getUserVisitsController.js';
-// import { updateVisitController } from '../controllers/ModulesService/Visits/updateVisitController.js';
+import { updateVisitController } from '../controllers/ModulesService/Visits/updateVisitController.js';
 
 // Creamos un router
 export const moduleRouter = express.Router();
@@ -21,7 +21,7 @@ moduleRouter.get('/module/:moduleId', moduleExist, getModuleController);
 
 //TODO: MODULO VISITS
 // Crear una nueva visita
-// moduleRouter.post('/user/module/visit', authenticateUser, newVisitController);
+moduleRouter.post('/user/module/visit', authenticateUser, newVisitController);
 
 // Listar todas las visitas usuario administrador
 // moduleRouter.get('/user/admin/module/visits', authenticateUser, adminAuthMiddleware,  getAdminVisitsController);
@@ -30,7 +30,7 @@ moduleRouter.get('/module/:moduleId', moduleExist, getModuleController);
 // moduleRouter.get('/user/module/visits', authenticateUser, getUserVisitsController);
 
 // Modificar una visita
-// moduleRouter.put('/user/module/visit/:visitId', authenticateUser, updateVisitController);
+moduleRouter.put('/user/module/visit/:visitId', authenticateUser, updateVisitController);
 
 // // Borrar una visita
 // moduleRouter.delete('/user/module/visit/:visitId', authenticateUser, deleteVisitController);

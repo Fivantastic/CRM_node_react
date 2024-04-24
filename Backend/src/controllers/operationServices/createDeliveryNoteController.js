28fda9585804c71503367ae0d76af3d8d2d0fc0b
Ivan <ivan.asencio34@gmail.com>
2024-04-24 17:59:11 +0200

    subiendo modificacion con la base de datos

--- Backend/src/controllers/operationServices/createDeliveryNoteController.js
+++ Backend/src/controllers/operationServices/createDeliveryNoteController.js
@@ -11,20 +11,39 @@ export const createDeliveryNoteController = async (req, res, next) => {
     // Validar el cuerpo de la solicitud
     await validateSchemaUtil(createDeliveryNoteSchema, req.body);
 
-    const { sale_id, deliverer_id, delivery_status, address_id, product_id } = req.body;
+    const {
+      sale_id,
+      deliverer_id,
+      delivery_status,
+      address_id,
+      saleProduct_id,
+    } = req.body;
 
     // Verificar si el producto tiene stock suficiente
-    const [product] = await pool.query(`SELECT stock FROM Products WHERE id_product = ?`, [product_id]);
+    const [product] = await pool.query(
+      `SELECT stock FROM Products WHERE id_product = ?`,
+      [saleProduct_id]
+    );
     const productStock = product[0]?.stock;
 
     if (productStock <= 0) {
       // Stock insuficiente, devuelve un error
-      res.status(400).json(errorResponse({ message: 'Stock insuficiente para el producto' }));
+      res
+        .status(400)
+        .json(
+          errorResponse({ message: 'Stock insuficiente para el producto' })
+        );
       return;
     }
 
     // Insertar la nota de entrega con los datos proporcionados
-    const noteId = await insertDeliveryNote(sale_id, deliverer_id, delivery_status, address_id, product_id);
+    const noteId = await insertDeliveryNote(
+      sale_id,
+      deliverer_id,
+      delivery_status,
+      address_id,
+      saleProduct_id
+    );
 
     // Responder con éxito y el ID del nuevo albarán
     res.json(success({ message: 'Albarán creado con éxito', noteId }));
--- Backend/src/schemas/createDeliveryNoteSchema.js
+++ Backend/src/schemas/createDeliveryNoteSchema.js
@@ -6,5 +6,5 @@ export const createDeliveryNoteSchema = Joi.object({
   deliverer_id: Joi.string().guid().required(), // ID del repartidor
   delivery_status: Joi.string().valid('pending', 'delivered').required(), // Estado de la entrega
   address_id: Joi.string().guid().required(), // ID de la dirección
-  product_id: Joi.string().guid() // Para el producto asociado, puede ser opcional
+  saleProduct_id: Joi.string().guid(), // Para el producto asociado, puede ser opcional
 }).messages(joiErrorMessages);

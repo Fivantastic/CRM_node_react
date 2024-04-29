fd5c7dd09296cef986399ba76120240568cd822c
Daniel Montero Castro <salamapatela@gmail.com>
2024-04-29 11:07:22 +0200

    correcion en la query de detalle de un modulo

--- Backend/src/models/Modules/selectModuleDetailModel.js
+++ Backend/src/models/Modules/selectModuleDetailModel.js
@@ -4,10 +4,9 @@ export const selectModuleDetailModel = async (moduleId) => {
   const pool = getDBPool();
 
   // Obtener el detalle del servicio
-  const result = await pool.query(
-    `SELECT * FROM Modules WHERE moduleId = ?`,
-    [moduleId]
-  );
+  const result = await pool.query(`SELECT * FROM Modules WHERE id_module = ?`, [
+    moduleId,
+  ]);
 
   return result[0];
 };

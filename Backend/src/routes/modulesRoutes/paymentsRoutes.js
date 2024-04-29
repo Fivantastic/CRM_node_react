65cf13ed27aed5f3f03128e42ef7e087e5042614
AlbertoGongora <gongorabada@gmail.com>
2024-04-29 14:52:44 +0200

    Agregada las rutas para los nuevos modulos

---
+++ Backend/src/routes/modulesRoutes/paymentsRoutes.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+export const paymentsRouter = express.Router();
+
+// Creacion de un pago
+
+// Cancelar un pago
+
+// Cerrar un pago
---
+++ Backend/src/routes/modulesRoutes/shipmentsRoutes.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+export const shipmentRouter = express.Router();
+
+// Creacion de un envio
+
+// Modificacion de un envio
+
+// Borrado de un envio
+
+// Completar un envio

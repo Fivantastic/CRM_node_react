6ea0cf38689a6dc297401623f300e8135c159e39
Ivan <ivan.asencio34@gmail.com>
2024-05-01 17:16:54 +0200

    separacion endpoint estructura MVC

--- Backend/src/schemas/Modules/deliveryNote/createDeliveryNoteSchema.js
+++ Backend/src/schemas/Modules/deliveryNote/createDeliveryNoteSchema.js
@@ -4,7 +4,7 @@ import { joiErrorMessages } from '../../error/joiErrorMessage.js';
 export const createDeliveryNoteSchema = Joi.object({
   sale_id: Joi.string().guid().required(), // ID de la venta asociada
   deliverer_id: Joi.string().guid().required(), // ID del repartidor
-  delivery_status: Joi.string().valid('pending', 'delivered').required(), // Estado de la entrega
   address_id: Joi.string().guid().required(), // ID de la dirección
+  customer_id: Joi.string().guid().required(), // ID de customer
   saleProduct_id: Joi.string().guid(), // Para el producto asociado, puede ser opcional
 }).messages(joiErrorMessages);
---
+++ Backend/src/schemas/Modules/deliveryNote/insertDeliveryNotesServices.js
@@ -0,0 +1,19 @@
+import { insertDeliveryNoteModel } from '../../../models/deliveryNote/deliveryNoteModels.js';
+
+export const createDeliveryNoteService = async (deliveryNoteData) => {
+  const { sale_id, deliverer_id, address_id, saleProduct_id } =
+    deliveryNoteData;
+
+  try {
+    await insertDeliveryNoteModel(
+      sale_id,
+      deliverer_id,
+      address_id,
+      saleProduct_id
+    );
+  } catch (error) {
+    error.statusCode = 500;
+    error.code = 'Error al insertar el albarán';
+    throw error;
+  }
+};

131c4295d8357a5af666cd4ce392bd7f771d4484
AlbertoGongora <gongorabada@gmail.com>
2024-05-02 13:04:43 +0200

    Correccion reset password y update password

--- Backend/src/controllers/users/resetPasswordController.js
+++ Backend/src/controllers/users/resetPasswordController.js
@@ -1,35 +1,18 @@
-import { success } from "../../utils/success.js";
-import { selectIdByRegistrationCode } from "../../models/user/selectIdByRegistrationCodeModel.js";
 import { updatePasswordService } from "../../services/user/updatePasswordService.js";
 import { validateSchemaUtil } from "../../utils/validateSchemaUtil.js";
 import { changeResetPasswordSchema } from "../../schemas/user/changeResetPasswordSchema.js";
+import { success } from "../../utils/success.js";
 
 export const resetPasswordController = async (req, res, next) => {
     try {
-        // Obtener el código de registro de la URL
-        const new_registration_code = req.params.registration_code;
-        
         // Validar el esquema del cuerpo de la solicitud
         await validateSchemaUtil(changeResetPasswordSchema, req.body);   
         
-        // Obtenemos el nuevo password
-        const { newPassword } = req.body;
-
-        // Obtenemos el id del usuario
-        const id_user  = await selectIdByRegistrationCode(new_registration_code);
-    
         // Actualizar la contraseña en la base de datos
-        await updatePasswordService(id_user , newPassword);
+        const response = await updatePasswordService(req.params.registration_code , req.body);
 
-        // Responder con éxito
-        res.json(success({ 
-            message: 'Contraseña cambiada con éxito', 
-            data: { 
-                id_user: id_user,
-                newPassword: newPassword
-             }
-        }));
-        
+        // Responder al cliente
+        res.json(success(response));
     } catch (error) {
         next(error);
     }

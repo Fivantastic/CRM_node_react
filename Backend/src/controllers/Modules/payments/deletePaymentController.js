4d70545a874378a105de9ca13d29aa1cfaf2fe9a
Daniel Montero Castro <salamapatela@gmail.com>
2024-05-03 10:10:12 +0200

    validaciones al eliminar y borrado de del proyecto eliminar un modulo

--- Backend/src/controllers/Modules/payments/deletePaymentController.js
+++ Backend/src/controllers/Modules/payments/deletePaymentController.js
@@ -1,5 +1,4 @@
-import { deletePaymentModel } from '../../../models/Modules/payments/deletePaymentModel.js';
-import { invalidCredentials } from '../../../services/error/errorService.js';
+import { selectPaymentService } from '../../../services/Modules/payments/selectPaymentService.js';
 import { success } from '../../../utils/success.js';
 
 export const deletePaymentController = async (req, res, next) => {
@@ -7,12 +6,8 @@ export const deletePaymentController = async (req, res, next) => {
     // Obtener el id del pago de la URL.
     const paymentsId = req.params.paymentsId;
 
-    // Eliminar el pago de la base de datos.
-    const deletePayment = await deletePaymentModel(paymentsId);
-
-    if (!deletePayment) {
-      invalidCredentials('El pago no existe');
-    }
+    // Verifico si esta cancelado
+    const deletePayment = await selectPaymentService(paymentsId);
 
     // Respondemos al cliente.
     res.status(200).send(success(deletePayment));

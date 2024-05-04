5ab1b09e5657203ed86eec2f9733108b0cfe8dd0
Ivan <ivan.asencio34@gmail.com>
2024-05-04 18:41:38 +0200

    Delivery implementado y funcionando

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,8 +1,8 @@
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, Link } from 'react-router-dom';
 import { HomePage } from './pages/Home/HomePage.jsx';
 import { NotFoundPage } from './pages/NotFoundPage.jsx';
 import { LoginPage } from './pages/LoginPage.jsx';
-import { RegisterPage } from './pages/UserRegistrationPage.jsx';
+import { RegisterPage } from './pages/UserRegistrationPage.jsx'; // Importa la página de registro
 import { ForgotPassword } from './pages/ForgotPassword.jsx';
 import { ResetPassword } from './pages/ResetPassword.jsx';
 import { SalesPage } from './pages/SalesPage.jsx';
@@ -18,6 +18,20 @@ function App() {
   return (
     <>
       <h1>Cosmic CRM</h1>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/login">Login</Link></li>
+          <li><Link to="/register">Register</Link></li> {/* Enlace para entrar en la página de registro */}
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/sale">Sales</Link></li>
+          <li><Link to="/visit">Visits</Link></li>
+          <li><Link to="/deliveryNote">Delivery Notes</Link></li>
+          <li><Link to="/invoice">Invoices</Link></li>
+          <li><Link to="/payment">Payments</Link></li>
+          <li><Link to="/shipment">Shipments</Link></li>
+        </ul>
+      </nav>
       <Routes>
         <Route path="/" element={<InitialPage />} />
         <Route path="/home" element={<HomePage />} />

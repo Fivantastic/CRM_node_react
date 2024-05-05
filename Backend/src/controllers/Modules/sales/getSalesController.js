9c915df88e54b21f718fea29c49d51db9f45703a
Daniel Montero Castro <salamapatela@gmail.com>
2024-05-05 07:31:20 +0200

    join de obtener todas las ventas

--- Backend/src/controllers/Modules/sales/getSalesController.js
+++ Backend/src/controllers/Modules/sales/getSalesController.js
@@ -1,11 +1,19 @@
 import { selectSalesListModel } from '../../../models/Modules/sales/selectSalesListModel.js';
-import { success } from '../../../utils/success.js';
+import { invalidCredentials } from '../../../services/error/errorService.js';
 
 export const getSalesController = async (req, res, next) => {
   try {
     const listSales = await selectSalesListModel();
 
-    res.status(200).send(success(listSales));
+    if (listSales === undefined) {
+      invalidCredentials('Error al obtener las ventas');
+    }
+
+    res.status(200).send({
+      status: 'ok',
+      message: 'Sales',
+      data: listSales,
+    });
   } catch (error) {
     next(error);
   }
--- Backend/src/models/Modules/sales/selectSalesListModel.js
+++ Backend/src/models/Modules/sales/selectSalesListModel.js
@@ -4,7 +4,13 @@ export const selectSalesListModel = async () => {
   const pool = getDBPool();
 
   // Obtener todas las ventas
-  const result = await pool.query(`SELECT * FROM Sales `);
+  const result =
+    await pool.query(`SELECT Users.name AS salesAgent, Users.last_name AS salesAgent_lastName, Products.name AS product_name, Products.price AS product_price, SalesProducts.quantity AS quantity, Customers.name AS customer, Customers.email AS customer_email, Customers.phone AS customer_phone, Sales.operation_status, Sales.create_at, Sales.update_at
+  FROM Sales
+  LEFT JOIN Users ON Sales.user_id = Users.id_user
+  LEFT JOIN SalesProducts ON Sales.saleProduct_id = SalesProducts.id_saleProduct
+  LEFT JOIN Products ON SalesProducts.product_id = Products.id_product
+  LEFT JOIN Customers ON Sales.customer_id = Customers.id_customer`);
 
   return result[0];
 };

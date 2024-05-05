0f3738a240fbdb5bfef7336d485fb1e7b165068b
Daniel Montero Castro <salamapatela@gmail.com>
2024-05-05 06:49:30 +0200

    obtener todas las ventas

--- Frontend/src/pages/SalesPage.jsx
+++ Frontend/src/pages/SalesPage.jsx
@@ -1,96 +1,50 @@
-import Joi from 'joi';
-import DynamicForm from '../components/forms/DynamicForm.jsx';
+import { useEffect, useState } from 'react';
+
 import { useUser } from '../context/authContext.jsx';
-import Swal from 'sweetalert2';
-import { Link } from 'react-router-dom';
+import { SalesList } from '../components/Sales/SalesList.jsx';
 
 export const SalesPage = () => {
   const token = useUser();
+  const [salesList, setSalesList] = useState([]);
 
-  const handleSaleSubmit = async (data) => {
-    try {
-      const response = await fetch('http://localhost:3000/sales/create', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `${token}`,
-        },
-        body: JSON.stringify(data),
-      });
-
-      if (response.ok) {
-        const responseData = await response.json();
-        console.log('Venta satisfactorio:', responseData);
-
-        //Opcion Modal 3
-        const Toast = Swal.mixin({
-          toast: true,
-          position: 'bottom-end',
-          showConfirmButton: false,
-          timer: 3000,
-          timerProgressBar: true,
-          didOpen: (toast) => {
-            toast.onmouseenter = Swal.stopTimer;
-            toast.onmouseleave = Swal.resumeTimer;
+  useEffect(() => {
+    const getSaleList = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/sales', {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `${token}`,
           },
         });
 
-        Toast.fire({
-          icon: 'success',
-          title: 'Venta Realizada con exito ! ',
-        });
-      } else {
-        const errorData = await response.json();
-        console.error('Venta fallido:', errorData);
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log('Obtener satisfactorio:', responseData);
+
+          // Actualizar el estado con los datos obtenidos
+          const dataArray = Object.values(responseData);
+          setSalesList(dataArray);
+        } else {
+          const errorData = await response.json();
+          console.error('Obetener fallido:', errorData);
+          // Mostrar un mensaje de error al usuario
+        }
+      } catch (error) {
+        console.error('Error al obtener la lista de ventas:', error);
         // Mostrar un mensaje de error al usuario
       }
-    } catch (error) {
-      console.error('Error durante la venta:', error);
-      // Mostrar un mensaje de error al usuario
-    }
-  };
+    };
 
-  const saleSchema = Joi.object({
-    id_user: Joi.string().required().min(36),
-    saleProduct_id: Joi.string().required().min(36),
-    customer_id: Joi.string().required().min(36),
-  });
-
-  const saleFormFields = [
-    {
-      name: 'id_user',
-      label: 'Usuario',
-      type: 'text',
-      placeholder: 'Introduce el usuario...',
-      required: true,
-    },
-    {
-      name: 'saleProduct_id',
-      label: 'Producto de venta',
-      type: 'text',
-      placeholder: 'Introduce el producto...',
-      required: true,
-    },
-    {
-      name: 'customer_id',
-      label: 'Cliente',
-      type: 'text',
-      placeholder: 'Introduce el cliente...',
-      required: true,
-    },
-  ];
+    getSaleList();
+  }, [token]); // Dependencia para que se ejecute cuando el token cambie
 
   return (
-    <div>
-      <li><Link to="/">Home</Link></li>
-      <DynamicForm
-        title="Crear Venta"
-        onSubmit={handleSaleSubmit}
-        schema={saleSchema}
-        fields={saleFormFields}
-        buttonText={'Crear'}
-        extraButtons={[]}
-      />
-    </div>
+    <>
+      <h1>Ventas</h1>
+      {salesList.map((data) => {
+        return <SalesList key={data.id} data={data} />;
+      })}
+    </>
   );
 };

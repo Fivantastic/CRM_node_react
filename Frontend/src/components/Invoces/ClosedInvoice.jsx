a71025b24bfde0c8281e8842718ef81e5e5d9c23
Daniel Montero Castro <salamapatela@gmail.com>
2024-05-06 13:01:54 +0200

    Forms de Facturas

---
+++ Frontend/src/components/Invoces/ClosedInvoice.jsx
@@ -0,0 +1,114 @@
+import Joi from 'joi';
+import DynamicFormPopUp from '../forms/DynamicFormPopUp.js';
+import Swal from 'sweetalert2';
+
+export const ClosedInvoice = ({ invoice, token }) => {
+  /* const token = useUser(); */
+  // Aqui hace la peticion al servidor
+  const handleclosedInvoiceAccion = async (formData) => {
+    try {
+      const response = await fetch(
+        `http://localhost:3000/invoice/close/${invoice}`,
+        {
+          method: 'PUT',
+          credentials: 'include',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `${token}`,
+          },
+          body: JSON.stringify(formData), // aqui va el formData lo que le envio lo del body
+        }
+      );
+
+      if (response.ok) {
+        //si la peticion es correcta
+        const responseData = await response.json();
+        console.log('Factura actualizada satisfactorio:', responseData);
+
+        // Aqui puedes mostrar un mensaje de exito con Swal que sale abajo a la derecha de la pantalla y dura 3 segundos
+        const Toast = Swal.mixin({
+          toast: true,
+          position: 'top-end',
+          showConfirmButton: false,
+          timer: 3000,
+          timerProgressBar: true,
+          didOpen: (toast) => {
+            toast.onmouseenter = Swal.stopTimer;
+            toast.onmouseleave = Swal.resumeTimer;
+          },
+        });
+
+        Toast.fire({
+          icon: 'success',
+          title: 'Actualización Realizada con exito ! ',
+        });
+      } else {
+        // si la peticion es incorrecta
+        const errorData = await response.json();
+        console.error('Actualización Factura fallido:', errorData);
+        // Aquí podrías mostrar un mensaje de error con Swal.fire si lo deseas
+      }
+    } catch (error) {
+      // si la peticion falla
+      console.error('Error durante la Actualización de factura:', error);
+      // Aquí podrías mostrar un mensaje de error con Swal.fire si lo deseas
+    }
+  };
+
+  // Titulo de la ventana, CAMBIARLO SI ES NECESARIO
+  const title = 'Cerrar Factura';
+
+  // Nombre que se muestra en el botón de submit, CAMBIARLO SI ES NECESARIO
+  const nameButton = 'Cerrar';
+
+  // Campos del formulario personalizables
+  const updateSaleFormFields = [
+    {
+      name: 'invoice_status',
+      label: 'Estado',
+      type: 'select',
+      options: [
+        { value: 'paid', label: 'Pagado' },
+        { value: 'overdue', label: 'Atrasada' },
+        { value: 'partially_paid', label: 'Parcialmente pagado' },
+        { value: 'cancelled', label: 'Cancelado' },
+        { value: 'refunded', label: 'Reintegrada' },
+        { value: 'disputed', label: 'Disputa' },
+        { value: 'sent', label: 'Enviado' },
+      ],
+    },
+  ];
+
+  // Esquema de validación, que sea el mismo que hay en la base de datos, solo cambiando lo de message por el label
+  const closedInvoiceSchema = Joi.object({
+    invoice_status: Joi.string()
+      .valid(
+        'pending',
+        'paid',
+        'overdue',
+        'partially_paid',
+        'cancelled',
+        'refunded',
+        'disputed',
+        'sent'
+      )
+      .required(),
+  });
+
+  // Crea el modal POP e inserta los campos y el esquema de validación, y luego retorna la informacion que tiene que introducir en el body
+  const handleClosedInvoice = () => {
+    DynamicFormPopUp(
+      title,
+      updateSaleFormFields,
+      closedInvoiceSchema,
+      handleclosedInvoiceAccion,
+      nameButton
+    );
+  };
+
+  return (
+    <div>
+      <button onClick={handleClosedInvoice}>Cerrar Factura</button>
+    </div>
+  );
+};
--- Frontend/src/pages/InvoicePage.jsx
+++ Frontend/src/pages/InvoicePage.jsx
@@ -1,10 +1,99 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import { CreateInvoice } from '../components/Invoces/CreateInvoice.jsx';
+import { InvoicesList } from '../components/Invoces/InvoicesList.jsx';
+import { ClosedInvoice } from '../components/Invoces/ClosedInvoice.jsx';
+import { DeleteGenericModal } from '../components/forms/DeleteGenericModal.jsx';
+import { useEffect, useState } from 'react';
+import { useUser } from '../context/authContext.jsx';
 
 export const InvoicePage = () => {
+  const token = useUser();
+  const [invoiceList, setInvoiceList] = useState([]);
+
+  // Tipo de Modulo para que la ruta URL de la peticion sea dinamica
+  const typeModule = 'invoice';
+
+  // Tipo de modulo para el nombre de los mensajes al cliente
+  const typeModuleMessage = 'Invoice';
+
+  useEffect(() => {
+    const getSaleList = async () => {
+      try {
+        const response = await fetch(`http://localhost:3000/${typeModule}`, {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `${token}`,
+          },
+        });
+
+        if (response.ok) {
+          const responseData = await response.json();
+          console.log('Obtener satisfactorio:', responseData);
+
+          // Actualizar el estado con los datos obtenidos
+          setInvoiceList(responseData.data);
+        } else {
+          const errorData = await response.json();
+          console.error('Obetener fallido:', errorData);
+          // Mostrar un mensaje de error al usuario
+        }
+      } catch (error) {
+        console.error('Error al obtener la lista de ventas:', error);
+        // Mostrar un mensaje de error al usuario
+      }
+    };
+
+    getSaleList();
+  }, [token]);
+
+  // Actualizo el estado con la venta añadida
+  const addInvoice = () => {
+    setInvoiceList((prevSales) => [...prevSales, invoiceList]);
+  };
+
+  // Actualizo el estado con la venta eliminada
+  const deleteInvoice = (id_invoice) => {
+    setInvoiceList((prevSales) =>
+      prevSales.filter((invoice) => invoice.id_invoice !== id_invoice)
+    );
+  };
+
+  // Actualizo el estado con la venta eliminada
+  const updateInvoice = (id_invoice) => {
+    setInvoiceList((prevSales) =>
+      prevSales.filter((invoice) => invoice.id_invoice !== id_invoice)
+    );
+  };
+
   return (
-    <>
-      <li><Link to="/">Home</Link></li>
-      <div>InvoicePage</div>
-    </>
+    <section className="invoice_container">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <h1 className="invoice_title">Facturas</h1>
+      <CreateInvoice onAddSale={addInvoice} token={token} />
+      <ol>
+        {invoiceList.map((data) => {
+          return (
+            <div key={data.id_invoice}>
+              <InvoicesList invoice={data} />
+              <ClosedInvoice
+                invoice={data.id_invoice}
+                onUpdateInvoice={updateInvoice}
+                token={token}
+              />
+              <DeleteGenericModal
+                id={data.id_invoice}
+                onDelete={deleteInvoice}
+                token={token}
+                typeModule={typeModule}
+                typeModuleMessage={typeModuleMessage}
+              />
+            </div>
+          );
+        })}
+      </ol>
+    </section>
   );
-}
+};

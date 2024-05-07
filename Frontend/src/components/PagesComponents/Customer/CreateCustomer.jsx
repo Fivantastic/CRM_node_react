894e6de5f47f1bfd9f6f63ce581c4fe93ecce86e
AlbertoGongora <gongorabada@gmail.com>
2024-05-07 11:42:31 +0200

    Orden de carpetas 2

--- Frontend/src/components/PagesComponents/Customer/CreateCustomer.jsx
+++ Frontend/src/components/PagesComponents/Customer/CreateCustomer.jsx
@@ -1,5 +1,5 @@
 import joi from 'joi';
-import DynamicFormPopUp from '../forms/DynamicFormPopUp.js';
+import DynamicFormPopUp from '../../forms/DynamicFormPopUp.js';
 import Swal from 'sweetalert2';
 
 export const CreateCustomer = ({ onAddCustomer, token }) => {

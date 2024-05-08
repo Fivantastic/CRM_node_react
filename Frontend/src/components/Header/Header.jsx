12eb6e6ebad4eed7243cb7b9e4a8597775443c8d
Daniel Montero Castro <salamapatela@gmail.com>
2024-05-08 23:54:01 +0200

    Modo dark y ligth

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,14 +1,15 @@
 // import { Magnifier } from "./Magnifier.jsx"
-import { ProfileHeader } from "./ProfileHeader.jsx";
+import { ProfileHeader } from './ProfileHeader.jsx';
 import './Magnifier.css';
 import './Header.css';
-
+import { ThemeSwicher } from '../ThemeSwicher.jsx';
 
 export const Header = () => {
-    return (
-        <header className="header-container">
-            {/* <Magnifier /> */}
-            <ProfileHeader />
-        </header>
-    )
-}
+  return (
+    <header className="header-container">
+      {/* <Magnifier /> */}
+      <ThemeSwicher />
+      <ProfileHeader />
+    </header>
+  );
+};

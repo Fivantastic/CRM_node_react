3ea15fe7a31da7b61854bf7ed516f89f10f9cc99
Juan Carlos Varela <jcvarelag@gmail.com>
2024-05-09 12:06:31 +0200

    productpage

--- Frontend/src/components/PagesComponents/Products/CreateProduct.jsx
+++ Frontend/src/components/PagesComponents/Products/CreateProduct.jsx
@@ -1,101 +1,105 @@
-// import Joi from 'joi';
-// import DynamicFormPopUp from '../../forms/DynamicFormPopUp.js';
-// import Swal from 'sweetalert2';
+import Joi from 'joi';
+import DynamicFormPopUp from '../../forms/DynamicFormPopUp.js';
+import Swal from 'sweetalert2';
 
-// export const CreateProduct = ({ onAddProduct, token }) => {  
-//     // Aqui hace la peticion al servidor
-//     const handleProductCreate = async (formData) => {
+export const CreateProduct = ({ onAddProduct, token }) => {  
+    // Aqui hace la peticion al servidor
+    const handleProductCreate = async (formData) => {
 
-//         try {
+        try {
            
-//             const response = await fetch('http://localhost:3000/products/new', {
-//                 method: 'POST',
-//                 credentials: 'include',
-//                 headers: {
-//                   'Content-Type': 'application/json',
-//                   Authorization: `${token}`, 
-//                 },
-//                 body: JSON.stringify(formData),
-//             });
+            const response = await fetch('http://localhost:3000/products/new', {
+                method: 'POST',
+                credentials: 'include',
+                headers: {
+                  'Content-Type': 'application/json',
+                  Authorization: `${token}`, 
+                },
+                body: JSON.stringify(formData),
+            });
 
 
-//             if (response.ok) {
-//                 //si la peticion es correcta
-//                 const responseData = await response.json();
-//                 console.log('Producto creado satisfactoriamente:', responseData);
+            if (response.ok) {
+                //si la peticion es correcta
+                const responseData = await response.json();
+                console.log('Producto creado satisfactoriamente:', responseData);
             
-//                 onAddProduct(responseData.data);
+                onAddProduct(responseData.data);
 
             
-//                 // Aqui puedes mostrar un mensaje de exito con Swal que sale abajo a la derecha de la pantalla y dura 3 segundos
-//                 const Toast = Swal.mixin({ 
-//                     toast: true,
-//                     position: 'bottom-end',
-//                     showConfirmButton: false,
-//                     timer: 3000,
-//                     timerProgressBar: true,
-//                     didOpen: (toast) => {
-//                     toast.onmouseenter = Swal.stopTimer;
-//                     toast.onmouseleave = Swal.resumeTimer;
-//                     },
-//                   });
+                // Aqui puedes mostrar un mensaje de exito con Swal que sale abajo a la derecha de la pantalla y dura 3 segundos
+                const Toast = Swal.mixin({ 
+                    toast: true,
+                    position: 'bottom-end',
+                    showConfirmButton: false,
+                    timer: 3000,
+                    timerProgressBar: true,
+                    didOpen: (toast) => {
+                    toast.onmouseenter = Swal.stopTimer;
+                    toast.onmouseleave = Swal.resumeTimer;
+                    },
+                  });
                   
-//                     Toast.fire({
-//                         icon: 'success',
-//                         title: 'Producto agregado con éxito',
-//                     });
-//             } else {
-//                 // Si la peticion es incorrecta
-//                 const errorData = await response.json();
-//                 console.error('Agregado de producto fallido', errorData);
-//             }
-//         } catch (error) {
-//             // Si la petición falla
-//             console.error('Error al agregar el producto')
-//         }
-//     };
-//     //Titulo de la venta
-//     const title = 'Agregar producto';
+                    Toast.fire({
+                        icon: 'success',
+                        title: 'Producto agregado con éxito',
+                    });
+            } else {
+                // Si la peticion es incorrecta
+                const errorData = await response.json();
+                console.error('Agregado de producto fallido', errorData);
+            }
+        } catch (error) {
+            // Si la petición falla
+            console.error('Error al agregar el producto')
+        }
+    };
+    //Titulo de la venta
+    const title = 'Agregar producto';
 
-//     // Nombre que se muestra en el botón submit.
-//     const nameButton = 'Agregar producto';
+    // Nombre que se muestra en el botón submit.
+    const nameButton = 'Agregar producto';
 
-//     // Campos del formulario personalizables
+    // Campos del formulario personalizables
 
-//     const ProductFormFields = [
+    const ProductFormFields = [
 
-//         {
-//             id_product: 'id_product',
-//             name: 'name',
-//             description: 'description',
-//             price: 'price',
-//             stock: 'stock',
-//             product_status: 'product_status',
-//             creation_at: 'creation_at',
-//             update_at: 'update_at',
-//         }
-//     ];
+        {
+            id_product: 'id_product',
+            name: 'name',
+            description: 'description',
+            price: 'price',
+            stock: 'stock',
+            product_status: 'product_status',
+            creation_at: 'creation_at',
+            update_at: 'update_at',
+        }
+    ];
 
-//     const newProductSchema = Joi.object({
-//         id_product: Joi.string().guid().required(),
-//     });
+    const newProductSchema = Joi.object({
+        name: Joi.string().guid().required(),
+        description: Joi.string().guid().required(),
+        price: Joi.string().guid().required(),
+        stock: Joi.number().required().min(1).max(100),
+        product_status: Joi.string().required().valid('active', 'inactive')
+    });
 
 
-//     const handleClickCreateProduct = () => {
-//         DynamicFormPopUp(
-//           title,
-//           ProductFormFields,
-//           newProductSchema,
-//           handleProductCreate,
-//           nameButton
-//         );
-//     };
+    const handleClickCreateProduct = () => {
+        DynamicFormPopUp(
+          title,
+          ProductFormFields,
+          newProductSchema,
+          handleProductCreate,
+          nameButton
+        );
+    };
 
-//     return (
-//         <div>
-//           <button onClick={handleClickCreateProduct}>Agregar producto</button>
-//         </div>
-//     );
+    return (
+        <div>
+          <button onClick={handleClickCreateProduct}>Agregar producto</button>
+        </div>
+    );
 
-// };
+};
     

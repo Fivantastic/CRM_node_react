03bc125e4a13289defbd9a1b204086eda5c95e3b
AlbertoGongora <150371451+AlbertoGongora@users.noreply.github.com>
2024-05-11 10:08:04 +0200

    Update Header.jsx

--- Frontend/src/components/Header/Header.jsx
+++ Frontend/src/components/Header/Header.jsx
@@ -1,20 +1,10 @@
-import { useDirection } from '@mantine/core';
 import { ThemeSwicher } from '../ThemeSwicher.jsx';
 import { ProfileNav } from '../ProfileNav/ProfileNav.jsx';
 import './Header.css';
 
 export const Header = () => {
-  const { toggleDirection, dir } = useDirection(); // Obtener la dirección y la función de cambio de dirección
-
   return (
     <header className="header-container">
-      {/* Botón de cambio de dirección */}
-      <button className="action-icon" onClick={() => toggleDirection()} aria-label="Toggle Direction">
-        {/* Icono de dirección según la dirección actual */}
-        <img className="iconReverse" src={dir === 'rtl' ? './loop.svg' : './loop.svg'} alt="Direction Icon" />
-      </button>
-
-      {/* Otros componentes del encabezado */}
       <ThemeSwicher />
       <ProfileNav />
     </header>

dc108c356a3ee4b5067929a50698f57096553f06
AlbertoGongora <150371451+AlbertoGongora@users.noreply.github.com>
2024-05-11 10:06:43 +0200

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,20 +3,15 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './context/authContext.jsx';
 import { ThemeProvider } from './context/ThemeContext.jsx';
-import { MantineProvider } from '@mantine/core'; // Importar MantineProvider
 import App from './App.jsx';
 import './index.css';
-
-const direction = 'rtl'; // 'rtl' o 'ltr'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthProvider>
       <BrowserRouter>
         <ThemeProvider>
-          <MantineProvider direction={direction}>
-            <App />
-          </MantineProvider>
+          <App />
         </ThemeProvider>
       </BrowserRouter>
     </AuthProvider>

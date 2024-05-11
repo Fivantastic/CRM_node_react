f00236986e589ef33df3482bea98192198db0a0a
AlbertoGongora <150371451+AlbertoGongora@users.noreply.github.com>
2024-05-11 10:07:25 +0200

    Update MainLayout.jsx

--- Frontend/src/layout/MainLayout.jsx
+++ Frontend/src/layout/MainLayout.jsx
@@ -2,7 +2,6 @@ import { useContext } from 'react';
 import { ThemeContext } from '../context/ThemeContext.jsx';
 import { Header } from '../components/Header/Header.jsx';
 import { NavBar } from '../components/NavBar/NavBar.jsx';
-import { DirectionProvider } from '@mantine/core';
 import './mainLayout.css';
 
 export function MainLayout({ children }) {
@@ -11,10 +10,8 @@ export function MainLayout({ children }) {
 
   return (
     <div className="main-layout-container">
-      <DirectionProvider initialDirection="rtl">
         <NavBar className="navbar-layout" />
         <Header className="header-layout" />
-    </DirectionProvider>
         <main className={`main-layout ${changeMode}`} id={`${changeMode}`}>
           {children}
         </main>

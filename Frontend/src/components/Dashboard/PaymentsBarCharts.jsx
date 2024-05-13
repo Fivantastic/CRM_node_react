f66841fe197834d765ac554f3ba4944f56e2c746
Daniel Montero Castro <salamapatela@gmail.com>
2024-05-13 21:47:08 +0200

    Inplementacion de loading ha las graficas

--- Frontend/src/components/Dashboard/PaymentsBarCharts.jsx
+++ Frontend/src/components/Dashboard/PaymentsBarCharts.jsx
@@ -15,55 +15,71 @@ import './Charts.css';
 export const PaymentsBarCharts = () => {
   const token = useUser();
   const [paymentsList, setPaymentsList] = useState([]);
+  const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
-    const getSaleList = async () => {
-      try {
-        const response = await fetch(`http://localhost:3000/payments/list`, {
-          method: 'GET',
-          headers: {
-            'Content-Type': 'application/json',
-            Authorization: `${token}`,
-          },
-        });
+  const getSaleList = async () => {
+    try {
+      const response = await fetch(`http://localhost:3000/payments/list`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `${token}`,
+        },
+      });
 
-        if (response.ok) {
-          const responseData = await response.json();
-          console.log('Obtener satisfactorio:', responseData);
+      if (response.ok) {
+        const responseData = await response.json();
+        console.log('Obtener satisfactorio:', responseData);
 
-          // Actualizar el estado con los datos obtenidos
-          setPaymentsList(responseData.data);
-        } else {
-          const errorData = await response.json();
-          console.error('Obetener fallido:', errorData);
-        }
-      } catch (error) {
-        console.error('Error al obtener la lista de pagos:', error);
+        // Actualizar el estado con los datos obtenidos
+        setPaymentsList(responseData.data);
+      } else {
+        const errorData = await response.json();
+        console.error('Obetener fallido:', errorData);
       }
-    };
-
+    } catch (error) {
+      console.error('Error al obtener la lista de pagos:', error);
+    }
+  };
+  useEffect(() => {
     getSaleList();
+    setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [token]);
 
+  useEffect(() => {
+    const error = console.error;
+    console.error = (...args) => {
+      if (/defaultProps/.test(args[0])) return;
+      error(...args);
+    };
+  }, []);
+
   const chartData = useMemo(
-    () => (paymentsList.length > 0 ? paymentsList : <p>Loading...</p>),
-    [paymentsList]
+    () => (loading ? <p>Loading...</p> : paymentsList),
+    [loading, paymentsList]
   );
 
   return (
     <>
       <section id="payments-charts">
         <h2 id="stock-charts">Pagos</h2>
-        <ResponsiveContainer>
-          <BarChart data={chartData} width={500} height={300}>
-            <CartesianGrid strokeDasharray="4 2 1" />
-            <XAxis dataKey="customer" />
-            <YAxis />
-            <Tooltip />
-            <Legend />
-            <Bar dataKey="paid_amount" fill="#0e8743" />
-          </BarChart>
-        </ResponsiveContainer>
+        {loading ? (
+          <div>Cargando...</div>
+        ) : (
+          <ResponsiveContainer>
+            <BarChart data={chartData} width={500} height={300}>
+              <CartesianGrid strokeDasharray="4 2 1" />
+              <XAxis dataKey="customer" />
+              <YAxis />
+              <Tooltip />
+              <Legend />
+              <Bar dataKey="paid_amount" fill="#0e8743" />
+            </BarChart>
+          </ResponsiveContainer>
+        )}
       </section>
     </>
   );

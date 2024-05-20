6fadd8ea53d9f2c63b448fb37cc8003dd5b5e133
Juan Carlos Varela <jcvarelag@gmail.com>
2024-05-20 20:36:56 +0200

    Ref_PR

--- Backend/src/models/products/selectProductSearchModel.js
+++ Backend/src/models/products/selectProductSearchModel.js
@@ -4,7 +4,7 @@ export const selectProductSearchModel = async (search) => {
   const pool = getDBPool();
   console.log(`Buscando en la base de datos con término: ${search}`);
   const [rows] = await pool.query(
-    'SELECT * FROM Products WHERE name LIKE? OR ref_PR LIKE?',
+    'SELECT * FROM Products WHERE name LIKE?  ref_PR LIKE?',
     [`%${search}%`, `%${search}`]
   );
   console.log(`Resultados encontrados: ${rows.length}`);
